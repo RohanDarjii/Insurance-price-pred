33f750b9e7bc1ea8cad43c262344ba38e228df1b
rohan darji <rohan.darji12397@gmail.com>
2023-02-15 13:39:38 +0530

    data transformation successfull

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from insurance.components.datavalidation import DataValidation
 from insurance.logger import logging
 from insurance.exception import InsuranceException
 from insurance.utils import get_collection_as_dataframe
-from insurance.entity.config_entity import DataIngestionConfig
+from insurance.entity.config_entity import DataIngestionConfig, TraningPipelineConfig
 from insurance.entity import config_entity
 from insurance.components.dataIngestion import DataIngestion
+from insurance.components.datatransformation import DataTransformation
 
 
 if __name__ == "__main__":
@@ -20,6 +21,11 @@ if __name__ == "__main__":
         data_validation_config = config_entity.DataValidationConfig(traning_pipeline_config= traning_pipeline_config)
         data_validation = DataValidation(data_validation_config= data_validation_config, data_ingestion_artifact= data_ingestion_artifact)
         data_validation_artifact = data_validation.initiate_data_validation()
+        #Data Transformation
+        data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config= traning_pipeline_config)
+        data_transformation = DataTransformation(data_transformation_config=data_transformation_config, 
+        data_ingestion_artifact=data_ingestion_artifact)
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
     except Exception as e:
-        raise InsuranceException(e, sys)
+          print(e)
 
