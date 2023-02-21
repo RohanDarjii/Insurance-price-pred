40b49874d87117f106a8948a05a5a4578eb38b0e
rohan darji <rohan.darji12397@gmail.com>
2023-02-21 21:42:03 +0530

    model pusher successfull

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os, sys
 from insurance.components.datavalidation import DataValidation
 from insurance.components.modelevaluation import ModelEvaluation
+from insurance.components.modelpusher import ModelPusher
 from insurance.components.modeltrainer import ModelTrainer
 from insurance.logger import logging
 from insurance.exception import InsuranceException
@@ -40,6 +41,12 @@ if __name__ == "__main__":
         data_transformation_artifact=data_transformation_artifact,
         model_trainer_artifact=model_trainer_artifact)
         model_eval_artifact = model_eval.initiate_model_evaluation()
+        # model pusher
+        model_pusher_config = config_entity.ModelPusherConfig(training_pipeline_config= traning_pipeline_config)
+        model_pusher = ModelPusher(model_pusher_config=model_pusher_config,
+                                        data_transformation_artifact=data_transformation_artifact,
+                                        model_trainer_artifact=model_trainer_artifact)
+        model_pusher_artifact = model_pusher.initiate_model_pusher()
     except Exception as e:
           print(e)
 
